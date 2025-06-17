807d31780bd9e960e39decbd41264c79a370fb11
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-17 22:28:59 +0300

    MigratorOptions.cs

---
+++ WindowsFormsAppRemaxProject/MigratorOptions.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyCache.Migrator
+{
+    public class MigratorOptions
+    {
+        [Option('q', "quiet", Required = false, HelpText = "Run in a non-interactive session")]
+        public bool Quiet { get; set; }
+
+        [Option('p', "path", Required = false, HelpText = "Folder path for csv-based seed data")]
+        public string Path { get; set; }
+    }
+}
