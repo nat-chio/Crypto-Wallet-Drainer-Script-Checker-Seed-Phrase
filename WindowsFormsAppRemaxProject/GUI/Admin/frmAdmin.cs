4e117dd8446e860dc3158cf51f80dab24227a08d
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-17 22:29:57 +0300

    frmAdmin.cs

---
+++ WindowsFormsAppRemaxProject/GUI/Admin/frmAdmin.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Guna.UI2.WinForms;
+using System.Data.SqlClient;
+
+
+
+namespace WindowsFormsAppRemaxProject
+{
+    public partial class frmAdmin : Form
+    {
+       
+        
+        public frmAdmin()
+        {
+
+        }
+
+       
+
+        private void btnHouses_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnClients_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEmployees_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserStatus_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlDesktop_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void wallet_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox14_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlTop_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
