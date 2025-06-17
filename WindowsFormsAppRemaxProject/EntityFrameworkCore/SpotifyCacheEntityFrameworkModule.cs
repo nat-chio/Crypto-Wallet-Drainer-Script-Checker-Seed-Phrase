09a4c8acb6cd3fc615a31eb90cc83b62840d5a54
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-17 22:29:17 +0300

    SpotifyCacheEntityFrameworkModule.cs

---
+++ WindowsFormsAppRemaxProject/EntityFrameworkCore/SpotifyCacheEntityFrameworkModule.cs
@@ -0,0 +1,50 @@
+﻿using Abp.EntityFrameworkCore.Configuration;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Abp.Zero.EntityFrameworkCore;
+using SpotifyCache.EntityFrameworkCore.Seed;
+
+namespace SpotifyCache.EntityFrameworkCore
+{
+    [DependsOn(
+        typeof(SpotifyCacheCoreModule), 
+        typeof(AbpZeroCoreEntityFrameworkCoreModule))]
+    public class SpotifyCacheEntityFrameworkModule : AbpModule
+    {
+        /* Used it tests to skip dbcontext registration, in order to use in-memory database of EF Core */
+        public bool SkipDbContextRegistration { get; set; }
+
+        public bool SkipDbSeed { get; set; }
+
+        public override void PreInitialize()
+        {
+            if (!SkipDbContextRegistration)
+            {
+                Configuration.Modules.AbpEfCore().AddDbContext<SpotifyCacheDbContext>(options =>
+                {
+                    if (options.ExistingConnection != null)
+                    {
+                        SpotifyCacheDbContextConfigurer.Configure(options.DbContextOptions, options.ExistingConnection);
+                    }
+                    else
+                    {
+                        SpotifyCacheDbContextConfigurer.Configure(options.DbContextOptions, options.ConnectionString);
+                    }
+                });
+            }
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(SpotifyCacheEntityFrameworkModule).GetAssembly());
+        }
+
+        public override void PostInitialize()
+        {
+            if (!SkipDbSeed)
+            {
+                SeedHelper.SeedHostDb(IocManager);
+            }
+        }
+    }
+}
