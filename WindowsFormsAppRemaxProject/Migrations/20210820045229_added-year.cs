06848e0a2358f4b5cf2e9e0500ff61ef01fe702c
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-17 22:31:35 +0300

    20210820045229_added-year.cs

---
+++ WindowsFormsAppRemaxProject/Migrations/20210820045229_added-year.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpotifyCache.Migrations
+{
+    public partial class addedyear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Year",
+                table: "Tracks",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Tracks");
+        }
+    }
+}
