9a92290d7b6a584b5f0b5d8f608e2ba53cf2ea74
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-17 22:29:19 +0300

    SpotifyCacheRepositoryBase.cs

---
+++ WindowsFormsAppRemaxProject/EntityFrameworkCore/Repositories/SpotifyCacheRepositoryBase.cs
@@ -0,0 +1,39 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Repositories;
+using Abp.EntityFrameworkCore;
+using Abp.EntityFrameworkCore.Repositories;
+
+namespace SpotifyCache.EntityFrameworkCore.Repositories
+{
+    /// <summary>
+    /// Base class for custom repositories of the application.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    /// <typeparam name="TPrimaryKey">Primary key type of the entity</typeparam>
+    public abstract class SpotifyCacheRepositoryBase<TEntity, TPrimaryKey> : EfCoreRepositoryBase<SpotifyCacheDbContext, TEntity, TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
+    {
+        protected SpotifyCacheRepositoryBase(IDbContextProvider<SpotifyCacheDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+
+        // Add your common methods for all repositories
+    }
+
+    /// <summary>
+    /// Base class for custom repositories of the application.
+    /// This is a shortcut of <see cref="SpotifyCacheRepositoryBase{TEntity,TPrimaryKey}"/> for <see cref="int"/> primary key.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    public abstract class SpotifyCacheRepositoryBase<TEntity> : SpotifyCacheRepositoryBase<TEntity, int>, IRepository<TEntity>
+        where TEntity : class, IEntity<int>
+    {
+        protected SpotifyCacheRepositoryBase(IDbContextProvider<SpotifyCacheDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+
+        // Do not add any method here, add to the class above (since this inherits it)!!!
+    }
+}
