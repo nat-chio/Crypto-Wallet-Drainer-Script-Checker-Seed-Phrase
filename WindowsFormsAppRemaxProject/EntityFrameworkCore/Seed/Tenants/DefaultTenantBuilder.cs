b14405567fa5337c3aee960af71bef630fc229ba
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-17 22:29:36 +0300

    DefaultTenantBuilder.cs

---
+++ WindowsFormsAppRemaxProject/EntityFrameworkCore/Seed/Tenants/DefaultTenantBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Abp.MultiTenancy;
+using SpotifyCache.Editions;
+using SpotifyCache.MultiTenancy;
+
+namespace SpotifyCache.EntityFrameworkCore.Seed.Tenants
+{
+    public class DefaultTenantBuilder
+    {
+        private readonly SpotifyCacheDbContext _context;
+
+        public DefaultTenantBuilder(SpotifyCacheDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create()
+        {
+            CreateDefaultTenant();
+        }
+
+        private void CreateDefaultTenant()
+        {
+            // Default tenant
+
+            var defaultTenant = _context.Tenants.IgnoreQueryFilters().FirstOrDefault(t => t.TenancyName == AbpTenantBase.DefaultTenantName);
+            if (defaultTenant == null)
+            {
+                defaultTenant = new Tenant(AbpTenantBase.DefaultTenantName, AbpTenantBase.DefaultTenantName);
+
+                var defaultEdition = _context.Editions.IgnoreQueryFilters().FirstOrDefault(e => e.Name == EditionManager.DefaultEditionName);
+                if (defaultEdition != null)
+                {
+                    defaultTenant.EditionId = defaultEdition.Id;
+                }
+
+                _context.Tenants.Add(defaultTenant);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
