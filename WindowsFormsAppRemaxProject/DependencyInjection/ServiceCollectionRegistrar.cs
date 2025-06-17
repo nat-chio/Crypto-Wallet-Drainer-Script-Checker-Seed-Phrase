10685df0e0514329423ad83bc9428f9ea36c457a
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-17 22:29:08 +0300

    ServiceCollectionRegistrar.cs

---
+++ WindowsFormsAppRemaxProject/DependencyInjection/ServiceCollectionRegistrar.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Castle.Windsor.MsDependencyInjection;
+using Abp.Dependency;
+using SpotifyCache.Identity;
+
+namespace SpotifyCache.Migrator.DependencyInjection
+{
+    public static class ServiceCollectionRegistrar
+    {
+        public static void Register(IIocManager iocManager)
+        {
+            var services = new ServiceCollection();
+
+            IdentityRegistrar.Register(services);
+
+            WindsorRegistrationHelper.CreateServiceProvider(iocManager.IocContainer, services);
+        }
+    }
+}
