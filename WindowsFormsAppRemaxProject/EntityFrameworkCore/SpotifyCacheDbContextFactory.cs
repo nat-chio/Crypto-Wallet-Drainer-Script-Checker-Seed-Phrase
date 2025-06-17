6440b260f55f3ae9ed06065480f9100e9e12e0ca
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-17 22:29:16 +0300

    SpotifyCacheDbContextFactory.cs

---
+++ WindowsFormsAppRemaxProject/EntityFrameworkCore/SpotifyCacheDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using SpotifyCache.Configuration;
+using SpotifyCache.Web;
+
+namespace SpotifyCache.EntityFrameworkCore
+{
+    /* This class is needed to run "dotnet ef ..." commands from command line on development. Not used anywhere else */
+    public class SpotifyCacheDbContextFactory : IDesignTimeDbContextFactory<SpotifyCacheDbContext>
+    {
+        public SpotifyCacheDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<SpotifyCacheDbContext>();
+            var configuration = AppConfigurations.Get(WebContentDirectoryFinder.CalculateContentRootFolder());
+
+            SpotifyCacheDbContextConfigurer.Configure(builder, configuration.GetConnectionString(SpotifyCacheConsts.ConnectionStringName));
+
+            return new SpotifyCacheDbContext(builder.Options);
+        }
+    }
+}
